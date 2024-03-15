9d02094e14aa30c4f50c0fbf6d07e2e531ce6d86
Ngọc ShinTran <163379246+ngocshintrannn@users.noreply.github.com>
2024-03-16 00:50:54 +0700

    Update setupCheckHost.sh

--- setupCheckHost.sh
+++ setupCheckHost.sh
@@ -3,6 +3,7 @@ apt install sudo -y
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install golang -y
 sudo apt install git -y
-git clone https://github.com/grafov/hulk.git && cd hulk
+git clone https://github.com/grafov/hulk.git
+cd hulk
 clear
 echo Dung lenh "go run hulk.go -site + webcandos" de chay!!

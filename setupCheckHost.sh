72273d78bde2d5515325bf71890d2f10e194da91
Ngọc ShinTran <163379246+ngocshintrannn@users.noreply.github.com>
2024-03-16 00:14:07 +0700

    Test code

--- setupCheckHost.sh
+++ setupCheckHost.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-#apt install sudo
-echo Hello world
+apt install sudo
+sudo apt update -y && sudo apt upgrade
+sudo apt install golang -y
+sudo apt install git -y
+git clone https://github.com/grafov/hulk.git && cd hulk
